8a2139815a346c9ad8c71d4c8401f9fe734c5c48
koolcid13 <avin.ahmadi23@ycdsbk12.ca>
2023-01-23 10:54:46 -0500

    upd constructor reader

--- src/cpt/dataReader.java
+++ src/cpt/dataReader.java
@@ -5,11 +5,15 @@ public class dataReader {
     private String strCountry;
     private double dblDeath1990;
     private double dblDeath2019;
+    private int intYear1;
+    private int intYear2;
 
-    public dataReader (String countryName, double deathRate1990, double deathRate2019) {
+    public dataReader (String countryName, int intYOne, double deathRate1990, double deathRate2019, int intYTwo) {
         this.strCountry = countryName;
         this.dblDeath1990 = deathRate1990;
         this.dblDeath2019 = deathRate2019;
+        this.intYear1 = intYOne;
+        this.intYear2 = intYTwo;
     }
 
     public String getCountry() {
