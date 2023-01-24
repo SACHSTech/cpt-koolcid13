409dac8ca1f51f25c95c2f89038eecd5622abd77
koolcid13 <avin.ahmadi23@ycdsbk12.ca>
2023-01-24 09:06:06 -0500

    sort class add + constructor and vars

---
+++ src/cpt/dataSort.java
@@ -0,0 +1,16 @@
+package cpt;
+
+import java.util.*;
+
+
+public class dataSort {
+    private ArrayList<dataPack> dataPoints;
+    private int size;
+
+    public dataSort(ArrayList<dataPack> dataPoints) {
+        this.dataPoints = dataPoints;
+        this.size = dataPoints.size();
+    }
+}
+
+
